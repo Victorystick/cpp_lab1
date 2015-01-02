6b57d4648832d24898f32b7a93a9215d91f7b0c8
Johan Fogelström <zapmew@gmail.com>
2015-01-02 18:24:49 +0100

    Implemented Hypercube completely. Needs tests though

--- Hypercube/Hypercube.hpp
+++ Hypercube/Hypercube.hpp
@@ -1,9 +1,22 @@
 #ifndef MP_Hypercube
 #define MP_Hypercube
 
-template <int Dimension>
-class Hypercube<Dimension> : public Vector< Hypercube<Dimension-1> > {};
+#include "../Vector/Vector.hpp"
 
+template <int Dimension>
+class Hypercube : public Vector<Hypercube<Dimension-1>> {
+
+public:
+	Hypercube<Dimension>(size_t size) : Vector<Hypercube<Dimension-1>>(size, Hypercube<Dimension-1>(size)) {}
+	Hypercube<Dimension-1> operator[](size_t i) {
+		return Vector<Hypercube<Dimension-1>>::operator[](i);
+	}
+	const Hypercube<Dimension-1> operator[](size_t i) const {
+		return Vector<Hypercube<Dimension-1>>::operator[](i);
+	}
+};
+
+template <>
 class Hypercube<1> : private Vector<int> {};
 
 #endif
