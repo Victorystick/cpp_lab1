dce23ac5d6ca3e9a3955ac66823832515dca17be
Victorystick <victorystick@gmail.com>
2015-01-02 13:59:16 +0100

    Implemented default-, identity- and copy-constructors

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -1,3 +1,27 @@
 #include "Matrix.h"
 
-Matrix::Matrix() {}
+Matrix::Matrix() : m_rows(0), m_cols(0) {}
+
+Matrix::Matrix(std::size_t size) : m_vectors(size), m_rows(size), m_cols(size) {
+	std::size_t i = 0;
+
+	for (Vector & v : m_vectors) {
+		v = Vector(size);
+
+		v[i] = 1;
+
+		++i;
+	}
+}
+
+Matrix::Matrix(const Matrix & m) : m_vectors(m.m_rows), m_rows(m.m_rows), m_cols(m.m_cols) {
+	for (std::size_t i = 0; i < m_rows; ++i) {
+		Vector & v = m_vectors[i];
+
+		v = Vector(m_cols);
+
+		for (std::size_t j = 0; j < m_cols; ++j) {
+			v[j] = m.m_vectors[i][j];
+		}
+	}
+}
