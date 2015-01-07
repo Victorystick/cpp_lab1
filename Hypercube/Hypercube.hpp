2962ac88a5c074bbd7d6339d6594bfeb668f33f4
Johan Fogelström <zapmew@gmail.com>
2015-01-07 15:53:39 +0100

    Fixed Hypercube implementation for use with example code.

--- Hypercube/Hypercube.hpp
+++ Hypercube/Hypercube.hpp
@@ -4,19 +4,24 @@
 #include "../Vector/Vector.hpp"
 
 template <int Dimension>
-class Hypercube : public Vector<Hypercube<Dimension-1>> {
+class Hypercube : private Vector<Hypercube<Dimension-1>> {
 
 public:
-	Hypercube<Dimension>(size_t size) : Vector<Hypercube<Dimension-1>>(size, Hypercube<Dimension-1>(size)) {}
-	Hypercube<Dimension-1> operator[](size_t i) {
+	Hypercube() : Vector<Hypercube<Dimension-1>>(0) {}
+	Hypercube(size_t size) : Vector<Hypercube<Dimension-1>>(size, Hypercube<Dimension-1>(size)) {}
+	Hypercube<Dimension-1> & operator[](size_t i) {
 		return Vector<Hypercube<Dimension-1>>::operator[](i);
 	}
-	const Hypercube<Dimension-1> operator[](size_t i) const {
+	const Hypercube<Dimension-1> & operator[](size_t i) const {
 		return Vector<Hypercube<Dimension-1>>::operator[](i);
 	}
 };
 
 template <>
-class Hypercube<1> : private Vector<int> {};
+class Hypercube<1> : public Vector<int> {
+	public:
+		Hypercube() : Vector<int>() {};
+		Hypercube(size_t size) : Vector<int>(size) {};
+};
 
 #endif
---
+++ Hypercube/hypercube_test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Hypercube.hpp"
+
+int main() {
+	Hypercube<3> n(7); // kub med 7*7*7 element
+	Hypercube<6> m(5); // sex dimensioner, 5*5*...*5 element
+	m[1][3][2][1][4][0] = 7;
+	Hypercube<3> t(5);
+	t = m[1][3][2]; // tilldela med del av m
+	t[1][4][0] = 2; // ändra t, ändra inte m
+	std::cout << m[1][3][2][1][4][0] << std::endl; // 7
+	std::cout << t[1][4][0] << std::endl; // 2
+}
